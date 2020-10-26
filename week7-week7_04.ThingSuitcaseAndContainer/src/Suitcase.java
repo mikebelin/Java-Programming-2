5f5c936a0ccd44ccbc826e9b3c99db3e1887f158
Mike Belinskiy <mikhail.belinskiy@gmail.com>
2020-10-26 19:41:15 +0200

    Add files via upload

---
+++ week7-week7_04.ThingSuitcaseAndContainer/src/Suitcase.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList; 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mishabobo
+ */
+public class Suitcase {
+        private int maxWeight;
+        private ArrayList<Thing> things; 
+        
+        //constructors
+        
+        
+        public Suitcase (int maxWeight){ 
+            
+        }
+        
+        public void addThing(Thing thing){
+            ArrayList<Thing> things = new ArrayList<Thing>();
+            
+        }
+        
+        public String toString(){
+            
+        
+        
+        
+}
